ed931d33ea9a48e9ee579bc3f0591b07e08b457e
Marcos Leite <m.abner@live.co.uk>
2022-11-16 16:33:47 -0300

    using absolut path

--- limparPDF.cmd
+++ limparPDF.cmd
@@ -1,37 +1,31 @@
 @echo off
 
 :: Alterar a pasta NFe de acordo com o cliente. E.g. NFE-1612
-cd %syslink%\NFe2
+set caminho=%syslink%\NFe2
 
 SET _letraUnidade=%syslink:~0,2%
-
-echo %_letraUnidade%
-:: Pause para teste
-pause
-
-
-
+echo Sistema esta instalado no drive %_letraUnidade%
 
 mkdir %temp%\PDFEspelho
-
 timeout /t 3
 
-echo "Copiando arquivos da pasta espelho..."
-copy /y Espelho\*.pdf %temp%\PDFEspelho 
-
+echo Copiando arquivos da pasta espelho...
+copy /y %caminho%\Espelho\*.pdf %temp%\PDFEspelho
 timeout /t 3
 
-echo "Apagando arquivos PDF da pasta NFe..."
-del /s /f *.pdf
-
+echo Apagando arquivos PDF da pasta NFe...
+del /s /f %caminho%\*.pdf
 timeout /t 3
 
-move %temp%\PDFEspelho\*.pdf Espelho\
+move %temp%\PDFEspelho\*.pdf %caminho%\Espelho\
+timeout /t 3
 
-echo "Limpeza da pasta concluida"
+mkdir /S /Q %temp%\PDFEspelho
+
+echo Limpeza da pasta concluida
 timeout /t 5
 
-timeout /t 3
+
 
 
 
