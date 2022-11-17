05e6c32da4840126375e61f4d6201f642b51442f
Marcos Leite <m.abner@live.co.uk>
2022-11-17 08:50:54 -0300

    removes unneeded timeouts

--- limparPDF.cmd
+++ limparPDF.cmd
@@ -7,22 +7,19 @@ SET _letraUnidade=%syslink:~0,2%
 echo Sistema esta instalado no drive %_letraUnidade%
 
 mkdir %temp%\PDFEspelho
-timeout /t 3
 
 echo Copiando arquivos da pasta espelho...
 copy /y %caminho%\Espelho\*.pdf %temp%\PDFEspelho
-timeout /t 3
 
 echo Apagando arquivos PDF da pasta NFe...
 del /s /f %caminho%\*.pdf
-timeout /t 3
 
 move %temp%\PDFEspelho\*.pdf %caminho%\Espelho\
-timeout /t 3
 
 mkdir /S /Q %temp%\PDFEspelho
 
-echo Limpeza da pasta concluida
+echo  Limpeza da pasta concluida 
+   
 timeout /t 5
 
 
